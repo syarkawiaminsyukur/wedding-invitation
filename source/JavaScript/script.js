685d40af43971340a75bd0711058764e41fce16f
Syarkawi amin syukur <anoni199zen@gmail.com>
2024-05-06 17:04:26 +0800

    membuat section story

--- source/JavaScript/script.js
+++ source/JavaScript/script.js
@@ -16,3 +16,14 @@ simplyCountdown(".simply-countdown", {
     return;
   },
 });
+
+const stickyTop = document.querySelector(".sticky-top");
+const offcanvas = document.querySelector(".offcanvas");
+
+offcanvas.addEventListener("show.bs.offcanvas", function () {
+  stickyTop.style.overflow = "visible";
+});
+
+offcanvas.addEventListener("hidden.bs.offcanvas", function () {
+  stickyTop.style.overflow = "hidden";
+});
