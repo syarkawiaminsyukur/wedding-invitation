e534ff4816725c4e23cf6ef63059f584086b763f
Syarkawi amin syukur <anoni199zen@gmail.com>
2024-05-07 15:32:11 +0800

    menampilkan nama pada undangan

--- source/JavaScript/script.js
+++ source/JavaScript/script.js
@@ -111,3 +111,4 @@ const pronaun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
 const namaContainer = document.querySelector(".hero h4 span");
 
 namaContainer.innerText = `${pronaun} ${nama},`.replace(/ ,$/, ",");
+document.querySelector("#Nama").value = nama;
